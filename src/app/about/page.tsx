4b60c096613c2bae0af9bb080bf29ffed5531e3d
jugonzalez <jugonzalez@hugeinc.com>
2025-04-01 16:10:04 -0500

    Add nes.css and tailwind-merge dependencies; update layout and styles; add About page and sound effect component

---
+++ src/app/about/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <main>
+ <Link className="nes-btn mb-10!" href="/">Go Back </Link>
+  <div className="nes-container is-dark with-title">
+    <p className="title">About</p>
+    <p>
+      I’m a passionate Software Developer with over 5 years of experience using
+      Web Development technologies. I’m a strong team player and a self-taught
+      individual contributor. I bring proactive and innovative initiatives from
+      inception to completion.
+    </p>
+    <p>
+      I have led successful software projects in various sectors, from online
+      advertising to the education area. I have the ability to adapt to new
+      environments and technologies easily and quickly. I take ownership of the
+      projects I work on and consistently seek out cutting-edge solutions and
+      the best engineering practices, following design patterns and
+      methodologies to deliver exceptional results. I am a result-oriented
+      developer who thrives in a collaborative environment, fostering a high
+      level of team spirit, accountability, and problem-solving skills. I am
+      committed to teaching and mentoring teammates.
+    </p>
+    <p>
+      My passion for reading and learning about AI, programming, and personal
+      growth keeps me up-to-date with industry trends and fuels my drive to
+      innovate and excel.
+    </p>
+  </div>    </main>
+  );
+}
