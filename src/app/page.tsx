27500d42e73609ba7784f405257b39fbbe013866
jugonzalez <jugonzalez@hugeinc.com>
2025-04-02 16:52:21 -0500

    feat: add Open Graph metadata and improve Home page structure with fragment wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,36 @@ import Link from "next/link";
 export const metadata: Metadata = {
   title: "Juan González 👾",
   description: "Senior Web Engineer at Kinesso",
+  openGraph: {
+    images: "/pixel_profile.png",
+  },
 };
 
 export default function Home() {
   return (
-    <main className="max-w-[1042px] mx-auto">
-      <BalloonCard
-        title="Home"
-        dialog="Hello! Welcome to my website. <br> <br> I'm Juan González, Senior Web Engineer at Kinesso. <br> <br> Feel free to navigate through the links."
-      ></BalloonCard>
+    <>
+      <main className="max-w-[1042px] mx-auto">
+        <BalloonCard
+          title="Home"
+          dialog="Hello! Welcome to my website. <br> <br> I'm Juan González, Senior Web Engineer at Kinesso. <br> <br> Feel free to navigate through the links."
+        ></BalloonCard>
 
-      <nav className="grid! grid-cols-2! gap-1! mt-8!">
-        <Link href="/about" className="nes-btn">
-          About
-        </Link>
-        <Link href="/projects" className="nes-btn">
-          Projects
-        </Link>
-        {/*
+        <nav className="grid! grid-cols-2! gap-1! mt-8!">
+          <Link href="/about" className="nes-btn">
+            About
+          </Link>
+          <Link href="/projects" className="nes-btn">
+            Projects
+          </Link>
+          {/*
         <Link href="/blog" className="nes-btn">
           Blog
         </Link> */}
-        <Link href="/contact" className="nes-btn">
-          Contact
-        </Link>
-      </nav>
-    </main>
+          <Link href="/contact" className="nes-btn">
+            Contact
+          </Link>
+        </nav>
+      </main>
+    </>
   );
 }
