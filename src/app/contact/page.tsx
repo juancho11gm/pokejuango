71e732d6cddbc28af2733b50de1c4d12d10c234a
jugonzalez <jugonzalez@hugeinc.com>
2025-04-02 15:36:19 -0500

    feat: add social media links section to Contact page with GitHub and LinkedIn profiles

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,6 +9,34 @@ export default function Page() {
       </Link>
 
       <BalloonCard title="Contact" dialog="Let's connect!" />
+      <section className="nes-container is-rounded is-dark with-title is-centered">
+        <p className="title">Social Media</p>
+
+        <div className="my-4!">
+          <ul className="flex! flex-col! gap-8!">
+            <li>
+              <Link
+                href="https://www.github.com/juancho11gm"
+                className="text-white! flex! items-center! gap-4"
+              >
+                <i className="nes-icon github is-large"></i>
+                <span>@juancho11gm</span>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href="https://www.linkedin.com/in/juansebastiangonzalezm/"
+                className="text-white! flex! items-center! gap-4"
+              >
+                <i className="nes-icon linkedin is-large"></i>
+
+                <span>/in/juansebastiangonzalezm</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
