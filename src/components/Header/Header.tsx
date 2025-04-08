9c53916ae4a54041a0f1f304d7fa84a0df956387
jugonzalez <jugonzalez@hugeinc.com>
2025-04-08 16:08:33 -0500

    feat: implement dynamic header visibility on scroll and add scroll-to-top button for improved navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,23 +2,55 @@
 
 import { SoundEffect } from "../SoundEffect/SoundEffect";
 import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const pathname = usePathname();
   const router = useRouter();
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector("header");
+      if (header) {
+        const headerRect = header.getBoundingClientRect();
+        setIsHeaderVisible(headerRect.top >= 0);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  console.log({ isHeaderVisible });
 
   const isHome = pathname === "/";
   return (
-    <header className="flex! justify-between! items-center! w-full! py-12!">
-      {isHome ? (
-        <span />
-      ) : (
-        <button className="nes-btn" onClick={() => router.back()}>
-          Go Back
+    <>
+      <header className="flex! justify-between! items-center! w-full! py-12!">
+        {isHome ? (
+          <span />
+        ) : (
+          <button className="nes-btn" onClick={() => router.back()}>
+            Go Back
+          </button>
+        )}
+
+        <SoundEffect />
+      </header>
+      {!isHeaderVisible && (
+        <button
+          onClick={scrollToTop}
+          className="fixed! bottom-8! right-8! nes-btn"
+          aria-label="Go to top"
+        >
+          ↑
         </button>
       )}
-
-      <SoundEffect />
-    </header>
+    </>
   );
 }
